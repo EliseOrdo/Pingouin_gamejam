a6452255c5fc24b861f7bd1abb3e2063de3aeacd
EliseOrdo <97050620+EliseOrdo@users.noreply.github.com>
2023-09-28 17:04:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import time
+
 
 pg.init()
 screen = pg.display.set_mode((1000, 800))
@@ -7,6 +7,12 @@ screen = pg.display.set_mode((1000, 800))
 background = pg.Surface(screen.get_size())
 background = background.convert()
 background.fill((186, 235, 239))
+
+mouvements = 10
+font = pg.font.Font(None, 24)
+text = font.render(str(mouvements), 1, (0, 100, 255))
+
+g = True
 
 
 class Pingouin:
@@ -16,18 +22,32 @@ class Pingouin:
         self.y = y
         self.prect = pg.Rect((x, y), (20, 40))
 
-    def move(self, touche):
-        vit = 22
-        if touche == pg.K_UP:
-            self.y -= vit
-        elif touche == pg.K_DOWN:
-            self.y += vit
-        elif touche == pg.K_RIGHT:
-            self.x += vit
-        elif touche == pg.K_LEFT:
-            self.x -= vit
-        self.prect = pg.Rect((self.x, self.y), (20, 40))
+    @staticmethod
+    def peut_bouger():
+        return mouvements > 0
 
+    def move(self, touche):
+        global mouvements
+        global g
+        vit = 22
+        if self.peut_bouger():
+            if touche == pg.K_UP:
+                self.y -= vit
+                mouvements -= 1
+            elif touche == pg.K_DOWN:
+                self.y += vit
+                mouvements -= 1
+            elif touche == pg.K_RIGHT:
+                self.x += vit
+                mouvements -= 1
+            elif touche == pg.K_LEFT:
+                self.x -= vit
+                mouvements -= 1
+            self.prect = pg.Rect((self.x, self.y), (20, 40))
+        else:
+            g = False
+            fon = pg.font.Font(None, 50)
+            screen.blit(fon.render("PERDUUUU", 1, (0, 100, 255)), (425, 350))
 
 ping = Pingouin(0, 0)
 runningf = True
@@ -40,5 +60,8 @@ while runningf:
     screen.blit(background, (0, 0))
     pg.draw.rect(screen, (250, 250, 250), ping.prect)
     pg.draw.rect(screen, (0, 0, 0), ping.prect, 1)
-    #time.sleep(0.5)
+    screen.blit(font.render(str(mouvements), 1, (0, 100, 255)), (960, 0))
+    if not g:
+        fon = pg.font.Font(None, 50)
+        screen.blit(fon.render("PERDUUUU", 1, (0, 100, 255)), (425, 350))
     pg.display.flip()
