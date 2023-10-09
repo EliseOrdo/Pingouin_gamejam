ffdaf9c34f9d345378c4b17b6f2c762e0cf6cee4
EliseOrdo <97050620+EliseOrdo@users.noreply.github.com>
2023-10-09 17:18:22 +0200

    Add files via upload

--- mainjeu_v2.py
+++ mainjeu_v2.py
@@ -39,10 +39,10 @@ class Pingouin:
         basg = x3[0] < y1[0] < x4[0] and x3[1] < y1[1] < y3[1]
         bd = x3[0] < y2[0] < x4[0] and x3[1] < y2[1] < y3[1]
         h = 0 > x1[1]
-        g = 0 > x1[0]
+        ga = 0 > x1[0]
         d = y2[0] > 1000
         b = y1[1] > 800
-        return hg or basg or hd or bd or h or g or d or b
+        return hg or basg or hd or bd or h or ga or d or b
 
     def touche_qui_ou(self):
         """Renvoie le mur touché par le pingouin, et sur quelle moitié de côté."""
@@ -135,14 +135,12 @@ class Liste:
 
     def make_listes(self):
         """fait les listes ."""
-        self.murs = [Mur(random.randint(0, 800), random.randint(0, 1000), (120, 140)) for j in
-                     range(random.randint(1, 10))]
+        self.murs = [Mur(random.randint(0, 800), random.randint(0, 1000), (120, 140)) for j in range(random.randint(1, 10))]
         self.pingouins = [Pingouin(random.randint(0, 800), random.randint(0, 1000)) for k in range(pingcibles)]
         self.cibles = [Cible(random.randint(0, 800), random.randint(0, 1000)) for i in range(pingcibles)]
         self.coll_pote(self.pingouins)
         self.coll_pote(self.murs)
         self.coll_pote(self.cibles)
-
 
     def coll_pote(self, obj):
         """Vérifie que la cible est dans une (autre) cible."""
@@ -150,10 +148,8 @@ class Liste:
             x1, x2 = (obj[i].x, obj[i].y), (obj[i].x + obj[i].taille[0], obj[i].y)
             y1, y2 = (obj[i].x, obj[i].y + obj[i].taille[1]), (obj[i].x + obj[i].taille[0], obj[i].y + obj[i].taille[1])
             for truc in range(len(self.cibles)):
-                x3, x4 = (self.cibles[truc].x, self.cibles[truc].y), (
-                self.cibles[truc].x + self.cibles[truc].taille[0], self.cibles[truc].y)
-                y3, y4 = (self.cibles[truc].x, self.cibles[truc].y + self.cibles[truc].taille[1]), (
-                self.cibles[truc].x + self.cibles[truc].taille[0], self.cibles[truc].y + self.cibles[truc].taille[1])
+                x3, x4 = (self.cibles[truc].x, self.cibles[truc].y), (self.cibles[truc].x + self.cibles[truc].taille[0], self.cibles[truc].y)
+                y3, y4 = (self.cibles[truc].x, self.cibles[truc].y + self.cibles[truc].taille[1]), (self.cibles[truc].x + self.cibles[truc].taille[0], self.cibles[truc].y + self.cibles[truc].taille[1])
                 hg = x3[0] < x1[0] < x4[0] and x3[1] < x1[1] < y3[1]
                 hd = x3[0] < x2[0] < x4[0] and x3[1] < x2[1] < y3[1]
                 basg = x3[0] < y1[0] < x4[0] and x3[1] < y1[1] < y3[1]
@@ -161,10 +157,8 @@ class Liste:
                 if hg or basg or hd or bd:
                     self.change(self.cibles, truc)
             for truc in range(len(self.murs)):
-                x3, x4 = (self.murs[truc].x, self.murs[truc].y), (
-                self.murs[truc].x + self.murs[truc].taille[0], self.murs[truc].y)
-                y3, y4 = (self.murs[truc].x, self.murs[truc].y + self.murs[truc].taille[1]), (
-                self.murs[truc].x + self.murs[truc].taille[0], self.murs[truc].y + self.murs[truc].taille[1])
+                x3, x4 = (self.murs[truc].x, self.murs[truc].y), (self.murs[truc].x + self.murs[truc].taille[0], self.murs[truc].y)
+                y3, y4 = (self.murs[truc].x, self.murs[truc].y + self.murs[truc].taille[1]), (self.murs[truc].x + self.murs[truc].taille[0], self.murs[truc].y + self.murs[truc].taille[1])
                 hg = x3[0] < x1[0] < x4[0] and x3[1] < x1[1] < y3[1]
                 hd = x3[0] < x2[0] < x4[0] and x3[1] < x2[1] < y3[1]
                 basg = x3[0] < y1[0] < x4[0] and x3[1] < y1[1] < y3[1]
@@ -172,11 +166,8 @@ class Liste:
                 if hg or basg or hd or bd:
                     self.change(self.murs, truc)
             for truc in range(len(self.pingouins)):
-                x3, x4 = (self.pingouins[truc].x, self.pingouins[truc].y), (
-                self.pingouins[truc].x + self.pingouins[truc].taille[0], self.pingouins[truc].y)
-                y3, y4 = (self.pingouins[truc].x, self.pingouins[truc].y + self.pingouins[truc].taille[1]), (
-                self.pingouins[truc].x + self.pingouins[truc].taille[0],
-                self.pingouins[truc].y + self.pingouins[truc].taille[1])
+                x3, x4 = (self.pingouins[truc].x, self.pingouins[truc].y), (self.pingouins[truc].x + self.pingouins[truc].taille[0], self.pingouins[truc].y)
+                y3, y4 = (self.pingouins[truc].x, self.pingouins[truc].y + self.pingouins[truc].taille[1]), (self.pingouins[truc].x + self.pingouins[truc].taille[0], self.pingouins[truc].y + self.pingouins[truc].taille[1])
                 hg = x3[0] < x1[0] < x4[0] and x3[1] < x1[1] < y3[1]
                 hd = x3[0] < x2[0] < x4[0] and x3[1] < x2[1] < y3[1]
                 basg = x3[0] < y1[0] < x4[0] and x3[1] < y1[1] < y3[1]
@@ -238,9 +229,8 @@ while runningf:
     # pg.draw.rect(screen, (0, 0, 0), ping.prect, 1)
     for pingind in range(len(Liste.pingouins)):
         if not Liste.pingouins[pingind].cache:
-            pg.draw.rect(screen, (250, 250, 250), pg.Rect((Liste.pingouins[pingind].x, Liste.pingouins[pingind].y),
-                                                          Liste.pingouins[pingind].taille))
-            pg.draw.rect(screen, (0, 0, 0), Liste.pingouins[pingind].prect, 1)
+            pg.draw.rect(screen, (250, 250, 250), pg.Rect((Liste.pingouins[pingind].x, Liste.pingouins[pingind].y), Liste.pingouins[pingind].taille))
+            pg.draw.rect(screen, (0, 0, 0), pg.Rect((Liste.pingouins[pingind].x, Liste.pingouins[pingind].y), Liste.pingouins[pingind].taille), 1)
     screen.blit(font.render(str(mouvements), 1, (0, 100, 255)), (960, 0))
 
     pg.display.flip()
