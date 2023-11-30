da5719b266faa5cbf0c06f128a7521bc7f4380ef
Choco <97348238+unetablettedechocolat@users.noreply.github.com>
2023-11-30 16:30:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame as pg
 import random
 import sys
+import time
 
 sys.setrecursionlimit(100000000)
 
@@ -21,13 +22,14 @@ class Pingouin:
     """définie un pingouin."""
 
     def __init__(self, x, y):
-        self.taille = (28, 29)
+        self.taille = (18, 17)
         # (width = horizontal = axe x, height = vertical = axe y)
         self.x = x
         self.y = y
         # Le point (x,y) est le point en haut à gauche de rectangle.
         self.prect = pg.Rect((self.x, self.y), self.taille)
         self.cache = False
+        self.orientation = 'haut'
 
     def touche_truc(self, truc):
         """Vérifie si le pinguoin touche le truc (le truc doit avoir x et y en paramètre)."""
@@ -62,19 +64,19 @@ class Pingouin:
         """Fait bouger le pingouin."""
         global mouvements
         global g
+        
         vit = 22
-        if self.touche_qui_ou() is False:
-
-            if touche == pg.K_UP:
-                self.y -= vit
-            elif touche == pg.K_DOWN:
-                self.y += vit
-            elif touche == pg.K_RIGHT:
-                self.x += vit
-            elif touche == pg.K_LEFT:
-                self.x -= vit
-
-            self.prect = pg.Rect((self.x, self.y), (20, 40))
+        #if self.touche_qui_ou() is False:
+            
+        #Mouvement
+        if touche == pg.K_UP:
+            self.y -= vit
+        elif touche == pg.K_DOWN:
+            self.y += vit
+        elif touche == pg.K_RIGHT:
+            self.x += vit
+        elif touche == pg.K_LEFT:
+            self.x -= vit            
 
         while self.touche_qui_ou() is True:
             if touche == pg.K_UP:
@@ -85,7 +87,68 @@ class Pingouin:
                 self.x -= 1
             elif touche == pg.K_LEFT:
                 self.x += 1
-            self.prect = pg.Rect((self.x, self.y), (20, 40))
+            self.prect = pg.Rect((self.x, self.y), (18, 17))
+            
+    def rotation(self, touche):
+        #Rotation
+        global pin
+        if self.orientation == 'haut':
+            if touche == pg.K_RIGHT:
+                self.orientation = 'droite'
+                pin = pg.transform.rotate(pin, -90)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_DOWN:
+                self.orientation = 'bas'
+                pin = pg.transform.rotate(pin, 180)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_LEFT:
+                self.orientation = 'gauche'
+                pin = pg.transform.rotate(pin, 90)
+                screen.blit(pin, (self.x, self.y))
+        
+        elif self.orientation == 'droite':
+            if touche == pg.K_UP:
+                self.orientation = 'haut'
+                pin = pg.transform.rotate(pin, 90)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_DOWN:
+                self.orientation = 'bas'
+                pin = pg.transform.rotate(pin, -90)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_LEFT:
+                self.orientation = 'gauche'
+                pin = pg.transform.rotate(pin, 180)
+                screen.blit(pin, (self.x, self.y))
+                
+        elif self.orientation == 'gauche':
+            if touche == pg.K_UP:
+                self.orientation = 'haut'
+                pin = pg.transform.rotate(pin, -90)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_DOWN:
+                self.orientation = 'bas'
+                pin = pg.transform.rotate(pin, 90)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_RIGHT:
+                self.orientation = 'droite'
+                pin = pg.transform.rotate(pin, 180)
+                screen.blit(pin, (self.x, self.y))
+
+        if self.orientation == 'bas':
+            if touche == pg.K_RIGHT:
+                self.orientation = 'droite'
+                pin = pg.transform.rotate(pin, 90)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_UP:
+                self.orientation = 'haut'
+                pin = pg.transform.rotate(pin, 180)
+                screen.blit(pin, (self.x, self.y))
+            elif touche == pg.K_LEFT:
+                self.orientation = 'gauche'
+                pin = pg.transform.rotate(pin, -90)
+                screen.blit(pin, (self.x, self.y))
+
+        self.prect = pg.Rect((self.x, self.y), (18, 17))
 
 
 class Mur:
@@ -107,12 +170,34 @@ class Cible:
         self.y = y
         self.couleur = (140, 220, 250)
         self.cache = False
+        self.anim = False
 
     def touche_cible(self, pingouin):
         """Renvoie vrai si le pingouin touche la cible."""
         if pingouin.touche_truc(self):
             self.cache = True
             pingouin.cache = True
+            self.anim = True
+            #But : soit mettre le pingouin au centre de la cible puis le faire disparaitre
+            #Met le pinguoin au centre de la cible (pas sûr ça, T)
+            #gauche
+            if pingouin.x<=self.x :
+                screen.blit(imcache, (pingouin.x-40, pingouin.y))
+                pg.display.update(pg.Rect(pingouin.x-40, pingouin.y, 40, 40))
+                
+            if pingouin.y<=self.y :
+                screen.blit(imcache, (pingouin.x, pingouin.y-40))
+                pg.display.update(pg.Rect(pingouin.x, pingouin.y-40, 40, 40))
+            else :
+                screen.blit(imcache, (pingouin.x, pingouin.y))
+                pg.display.update(pg.Rect(pingouin.x, pingouin.y, 40, 40))
+            
+            screen.blit(ci, (self.x, self.y))
+            print(pingouin.x, pingouin.y)
+            print(self.x, self.y)
+            pg.display.update(pg.Rect(self.x, self.y, 40, 40))
+
+           
 
 
 def coll_pote(obj):
@@ -131,7 +216,6 @@ def coll_pote(obj):
             bd = x3[0] < y2[0] < x4[0] and x3[1] < y2[1] < y3[1]
             if hg or basg or hd or bd:
                 change(liste_cibles, cib)
-                
         for c in range(len(liste_murs)):
             x3, x4 = (liste_murs[c].x, liste_murs[c].y), (liste_murs[c].x + liste_murs[c].taille[0], liste_murs[c].y)
             y3, y4 = (liste_murs[c].x, liste_murs[c].y + liste_murs[c].taille[1]), (
@@ -142,7 +226,6 @@ def coll_pote(obj):
             bd = x3[0] < y2[0] < x4[0] and x3[1] < y2[1] < y3[1]
             if hg or basg or hd or bd:
                 change(liste_murs, c)
-                
         for truc in range(len(liste_pingouins)):
             x3, x4 = (liste_pingouins[truc].x, liste_pingouins[truc].y), (
                 liste_pingouins[truc].x + liste_pingouins[truc].taille[0], liste_pingouins[truc].y)
@@ -155,33 +238,7 @@ def coll_pote(obj):
             bd = x3[0] < y2[0] < x4[0] and x3[1] < y2[1] < y3[1]
             if hg or basg or hd or bd:
                 change(liste_pingouins, truc)
-                
 
-# pg.Rect.colliderect(Rect) pour collisions entre 2 rectangles pas penchés
-
-"""def collautres(obj , liste):
-    if len(liste)<=1:
-        return False
-    for elt in liste[1::]:
-        if pg.Rect(obj.x,obj.y,obj.taille).colliderect(elt):
-            return True
-    return False"""
-    
-def coll(obj, liste):
-    x1, x2 = (obj.x, obj.y), (obj.x + obj.taille[0], obj.y)
-    y1, y2 = (obj.x, obj.y + obj.taille[1]), (obj.x + obj.taille[0], obj.y + obj.taille[1])
-    for cib in range(len(liste)):
-        x3, x4 = (liste[cib].x, liste[cib].y), (
-            liste[cib].x + liste[cib].taille[0], liste[cib].y)
-        y3, y4 = (liste[cib].x, liste[cib].y + liste[cib].taille[1]), (
-            liste[cib].x + liste[cib].taille[0], liste[cib].y + liste[cib].taille[1])
-        hg = x3[0] < x1[0] < x4[0] and x3[1] < x1[1] < y3[1]
-        hd = x3[0] < x2[0] < x4[0] and x3[1] < x2[1] < y3[1]
-        basg = x3[0] < y1[0] < x4[0] and x3[1] < y1[1] < y3[1]
-        bd = x3[0] < y2[0] < x4[0] and x3[1] < y2[1] < y3[1]
-        if hg or basg or hd or bd:
-            #change(liste_cibles, cib)
-            return True
 
 def change(liste, ind):
     """Change les coordonnées de la cible à changer."""
@@ -202,62 +259,26 @@ def change(liste, ind):
 
 
 # pingcibles = random.randint(1, 10)
-pingcibles = 3
+pingcibles = 4
 
-pin = pg.image.load("ping.png").convert_alpha()
-ci = pg.image.load("water.png").convert_alpha()
-ice = pg.image.load("iceberg.png").convert_alpha()
-wallpaper = pg.image.load("wallpapers_neige.png").convert_alpha()
+pin = pg.image.load("dessins/ping.png").convert_alpha()
+ci = pg.image.load("dessins/water.png").convert_alpha()
+ice = pg.image.load("dessins/iceberg.png").convert_alpha()
+wallpaper = pg.image.load("dessins/wallpapers_neige.png").convert_alpha()
+ci1 = pg.image.load("dessins/t1.png").convert_alpha()
+ci2 = pg.image.load("dessins/t2.png").convert_alpha()
+ci3 = pg.image.load("dessins/t3.png").convert_alpha()
+ci4 = pg.image.load("dessins/t4.png").convert_alpha()
+imcache = pg.image.load("dessins/snow.png").convert_alpha()
 
 # Fait les listes
 
-def lm(n):
-    li = []
-    while len(li) <= n:
-        ajout = True
-        m = Mur(random.randint(0, 800), random.randint(0, 1000), (120, 140))
-        for j in range(len(li)):
-            if coll(m, li):
-                ajout = False
-        if ajout:
-            li.append(m)
-    return li
-
-def lc(n):
-    li = []
-    while len(li) < n:
-        ajout = True
-        m = Cible(random.randint(0, 800), random.randint(0, 1000))
-        for j in range(len(li)):
-            if coll(m, li) or coll(m, liste_murs):
-                ajout = False
-        if ajout:
-            li.append(m)
-    return li
-
-def lp(n):
-    li = []
-    while len(li) < n:
-        ajout = True
-        m = Pingouin(random.randint(0, 800), random.randint(0, 1000))
-        for j in range(len(li)):
-            if coll(m, li) or coll(m, liste_murs) or coll(m, liste_cibles):
-                ajout = False
-        if ajout:
-            li.append(m)
-    return li
-
-
-liste_murs = lm(5)
-liste_cibles = lc(pingcibles)
-liste_pingouins = lp(pingcibles)
-
-#liste_murs = [Mur(random.randint(0, 800), random.randint(0, 1000), (120, 140)) for j in range(random.randint(1, 2)) if not collautres(liste_murs[j], liste_murs)]
-#liste_pingouins = [Pingouin(random.randint(0, 800), random.randint(0, 1000)) for k in range(pingcibles)]
-#liste_cibles = [Cible(random.randint(0, 800), random.randint(0, 1000)) for i in range(pingcibles)]
-#coll_pote(liste_pingouins)
-#coll_pote(liste_murs)
-#coll_pote(liste_cibles)
+liste_murs = [Mur(random.randint(0, 800), random.randint(0, 1000), (119, 129)) for j in range(random.randint(1, 3))]
+liste_pingouins = [Pingouin(random.randint(0, 800), random.randint(0, 1000)) for k in range(pingcibles)]
+liste_cibles = [Cible(random.randint(0, 800), random.randint(0, 1000)) for i in range(pingcibles)]
+coll_pote(liste_pingouins)
+coll_pote(liste_murs)
+coll_pote(liste_cibles)
 
 runningf = True
 while runningf:
@@ -267,6 +288,7 @@ while runningf:
             runningf = False
         if event.type == pg.KEYDOWN:
             mouvements += 1
+            liste_pingouins[0].rotation(event.key)
             for pingind in range(len(liste_pingouins)):
                 if not liste_pingouins[pingind].cache:
                     liste_pingouins[pingind].move(event.key)
@@ -279,11 +301,28 @@ while runningf:
     for ciblind in liste_cibles:
         if not ciblind.cache:
             screen.blit(ci, (ciblind.x, ciblind.y))
+        if ciblind.anim:
+            screen.blit(ci1, (ciblind.x, ciblind.y))
+            pg.display.update((ciblind.x, ciblind.y, 40, 40))
+            time.sleep(0.3)
+            screen.blit(ci2, (ciblind.x, ciblind.y))
+            pg.display.update((ciblind.x, ciblind.y, 40, 40))
+            time.sleep(0.3)
+            screen.blit(ci3, (ciblind.x, ciblind.y))
+            pg.display.update((ciblind.x, ciblind.y, 40, 40))
+            time.sleep(0.3)
+            screen.blit(ci4, (ciblind.x, ciblind.y))
+            pg.display.update((ciblind.x, ciblind.y, 40, 40))
+            time.sleep(0.3)
+            screen.blit(imcache, (ciblind.x, ciblind.y))
+            pg.display.update((ciblind.x, ciblind.y, 40, 40))
+            ciblind.anim = False
     for murind in range(len(liste_murs)):
         screen.blit(ice, (liste_murs[murind].x, liste_murs[murind].y))
     for pingind in range(len(liste_pingouins)):
         if not liste_pingouins[pingind].cache:
             screen.blit(pin, (liste_pingouins[pingind].x, liste_pingouins[pingind].y))
+            print(liste_pingouins[pingind].x, liste_pingouins[pingind].y)
     screen.blit(font.render(str(mouvements), 1, (0, 100, 255)), (960, 0))
     pg.display.flip()
 pg.quit()
