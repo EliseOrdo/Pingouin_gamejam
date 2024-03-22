2efcdfb9c743cb1904f90b0c9047f49bafbfeaa4
EliseOrdo <elise.ordoquy@icloud.com>
2024-03-22 15:26:55 +0100

    Revert "réglage de d du commit d'avant"
    This reverts commit 23e8f8b221f328f5461c5d57a9f023a84bfaa8ad.

--- main.py
+++ main.py
@@ -375,17 +375,8 @@ def coll(obj, liste):
 
 
 # pingcibles = random.randint(1, 10)
-pingcibles = 5
+pingcibles = 3
 
-pin = pg.image.load("dessins/ping.png").convert_alpha()
-cache = pg.image.load("dessins/snow.png").convert_alpha()
-ci = pg.image.load("dessins/water.png").convert_alpha()
-ice = pg.image.load("dessins/iceberg.png").convert_alpha()
-wallpaper = pg.image.load("dessins/wallpapers_neige.png").convert_alpha()
-ci1 = pg.image.load("dessins/t1.png").convert_alpha()
-ci2 = pg.image.load("dessins/t2.png").convert_alpha()
-ci3 = pg.image.load("dessins/t3.png").convert_alpha()
-ci4 = pg.image.load("dessins/t4.png").convert_alpha()
 
 # Fait les listes
 
