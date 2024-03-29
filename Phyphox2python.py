a5abc9c3d911547a589cf2424f46185fd720527d
une_tablette_de_chocolat <tiphaine.ms@free.fr>
2024-03-29 17:28:20 +0100

    on passe en accéléromètre

--- Phyphox2python.py
+++ Phyphox2python.py
@@ -11,7 +11,7 @@ import time
 
 
 # A changer à chaques fois
-PP_ADDRESS = "http://192.168.201.140:8080" 
+PP_ADDRESS = "http://192.168.76.40" 
 
 fig, (ax1, ax2,ax3) = plt.subplots(ncols=3)
 
@@ -20,9 +20,10 @@ def Monitor(PP_ADDRESS):
      PP_CHANNELS = ["pressure"]
      PP_CHANNELS = ["pCal","tempCal"] #For the sensor tag (also a version with Raw instead of 
      cal but the unites are weird)
-     PP_CHANNELS = ["accX","accY","accZ"] #pour l'accélération"""
-     PP_CHANNELS = ["gyrX","gyrY","gyrZ"] #pour le gyroscope
-     
+     PP_CHANNELS = ["gyrX","gyrY","gyrZ"] #pour le gyroscope"""
+
+     PP_CHANNELS = ["accX","accY","accZ"] #pour l'accélération
+
      starturl = PP_ADDRESS + "/control?cmd=start"
      
      '''Connect to the phone by wifi'''
@@ -32,49 +33,60 @@ def Monitor(PP_ADDRESS):
      p3=0
      
      """ initialisation du tableau de données"""
-     depart = time.time()
+     dep = time.time()
      m = 1 # masse des pingouins ( en faire des plus lourd ?)
-     g = 1 # constante accélération de gravite pour simuler le poids 
      pos_p = [0,0] #self.x, self.y
      vit_p = [0,0]
-     ang_p = [0,0] 
-     coo_gyr = np.array([[0],[0],[0]]) #x,y,z
+     acc = [0,0,0] #(x,y,z)    liste car pas besoin d'une array
+     #x(phyphox)=y(écran)
+     print("--------------GO-------------")
      while True: # une fois dans le main, à enlever et à mettre dans la boucle principale
          #PP_ADRESS/get?&
          url = PP_ADDRESS + "/get?" + ("&".join(PP_CHANNELS))
          data = requests.get(url=url).json()
-         duree = time.time() - depart
          for i, channel in enumerate(PP_CHANNELS):
              value = data["buffer"][channel]["buffer"][0]
-             """
+             if(value == None): value = 0  #si value==None, on ne peut pas la mettre dans notre array acc_p
+             print ('Channel is : {}, value is : {} ,index is : {}'.format(channel,value,i) )
+             if(value<=-5 or value>=5 ):
+                acc[i] = value/5
+             else:
+                acc[i] = 0
+             print(acc[i])
+             print("pingouin : \nax : {}\nay : {}\n".format(acc[0],acc[1]))
+             #on n'oublie pas de séparer les cas i=0 ou 1 parce que sinon le truc fait deux fois les additions et évidemment ça part dans e130
+             if i ==0 :
+                if(vit_p[0] < 1 and vit_p[0]> -1): #0.5*0.5=0.25 on évite que ça tende vers 0 à l'infini
+                    vit_p[0] = acc[1]
+                else :
+                    vit_p[0] *= acc[1] #v = v*a
+             if i ==1 :
+                if(vit_p[1] < 1 and vit_p[1]> -1): 
+                    vit_p[1] = acc[0]
+                else : 
+                    vit_p[1] *= acc[0] #on inverse x et y ici
+             print("pingouin : \nvx : {}\nvy : {}\n".format(vit_p[0],vit_p[1]))
+             if i ==0 :
+                pos_p[0] += vit_p[0]  #x = x+v
+             if i ==1 :
+                pos_p[1] += vit_p[1]   
+             print("pingouin : \npx : {}\npy : {}\n".format(pos_p[0],pos_p[1]))
+            
              if i ==0 : 
-                 ax1.plot(p1,value,'ro')
+                 ax1.plot(p1,pos_p[0],'ro')
                  plt.pause(0.005)
                  p1+=1
              if i ==1 : 
-                 ax2.plot(p2,value,'go')
+                 ax2.plot(p2,pos_p[1],'go')
                  plt.pause(0.005)
                  p2+=1
-             if i ==2 : 
+             """if i ==2 : 
                  ax3.plot(p3,value,'bo')
                  plt.pause(0.005)
-                 p3+=1
-                 """
-             print ('Channel is : {}, value is : {} ,index is : {}'.format(channel,value,i) )
-             coo_gyr[i][0] = value
-             print(coo_gyr[i][0])
-             ang_p[0] = coo_gyr[0][0] * duree  # acc_angle* duree = angle : teta
-             ang_p[1] = coo_gyr[1][0] * duree
-             print("pengouin : \nax : {}\nay : {}\n".format(ang_p[0],ang_p[1]))
-             vit_p[0] = -ang_p[0] + vit_p[0] # -angle car les axes y de phyphox et de l'écran sont inversés
-             vit_p[1] = coo_gyr[1][0]+vit_p[1] 
-             print("pengouin : \nvx : {}\nvy : {}\n".format(vit_p[0],vit_p[1]))
-             pos_p[0] = coo_gyr[0][0]*vit_p[0] + pos_p[0] 
-             pos_p[1] = coo_gyr[1][0]*vit_p[1] + pos_p[1]   
-             print("pengouin : \npx : {}\npy : {}\n".format(pos_p[0],pos_p[1]))
-             ax1.plot(duree,vit_p[0],'bo')
-             plt.pause(0.005)
-             
+                 p3+=1 """
 
             #time.sleep(0.05)
 Monitor(PP_ADDRESS)
+
+
+#trucs à expliquer : vitesse angulaire, on veut pouvoir pencher lentement et que ça glisse quand même, donc on modélise le poids
